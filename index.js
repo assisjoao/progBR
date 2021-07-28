05bead9cf4e88d0d2987e0f643de516003e65574
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-07-28 20:58:51 -0300

    With POST, PUT and DELETE

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ const app = express();
 app.use("/meusite", express.static(path.join(__dirname, 'client')));
 
 app.get("/", (req, res) => {
-    // res.set("Content-Type", "text/plain")
-    res.type("txt");
+    
     res.send("<h1>Hello Hello GET</h1>");
 });
+
+app.post("/", (req, res) => {
+    res.send("<h1>POST</h1>");
+});
+
+app.put("/", (req, res) => {
+    res.send("<h1>PUT</h1>");
+});
+
+app.delete("/", (req, res) => {
+    res.send("<h1>DELETE</h1>");
+});
+
 
 const PORT = 5000;
 app.listen(PORT, ()=>{
