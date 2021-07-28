661526b8e6419e74e24957fbc1e1fe9d248d842d
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-07-28 20:42:57 -0300

    With static files

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client')));
 
 const PORT = 5000;
 app.listen(PORT, ()=>{
