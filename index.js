6ffcd077862cd676fc27bf81bf803bce8d874764
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-07-28 20:27:34 -0300

    With Node Express

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
 
-function handleFile (req, res) {
-    let path = url.parse(req.url).pathname;
-}
+const app = express();
 
-http.createServer((request, response) =>{
-
-    let fileName = "." + path;
-
-    fs.readFile(fileName, (err, data) => {
-        if (err){
-            response.writeHead(404, {"Content-Type": "text/html;charset=utf-8"});
-            response.end("<h1>Página Não Encontrada</h1>")
-        } else {
-            response.writeHead(200, {"Content-Type": "text/html"});
-            response.write(data);
-            response.end();
-        }
-    })
-
-    
-    response.end();
-
-}).listen(3000, (err)=>{
-    if (err){
-    console.log(err)
-    } else {
-        console.log("Servidor Rodando na porta 3000")
-    }
-})
+const PORT = 5000;
+app.listen(PORT, ()=>{
+    console.log(`Server Running on Port:${PORT}`)
+});
