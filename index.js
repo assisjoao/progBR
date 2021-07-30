d336270ad3122b91262a570daf5de56ee2a2eda2
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-07-30 18:31:29 -0300

    New version day 30/07

--- index.js
+++ index.js
@@ -7,22 +7,20 @@ app.use("/meusite", express.static(path.join(__dirname, 'client')));
 
 app.get("/", (req, res) => {
     
-    res.send("<h1>Hello Hello GET</h1>");
+    res.send("<h1>Hello GET</h1>");
 });
 
 app.post("/", (req, res) => {
-    res.send("<h1>POST</h1>");
+    res.send("<h1>POST!</h1>");
 });
 
 app.put("/", (req, res) => {
-    res.send("<h1>PUT</h1>");
+    res.send("<h1>PUT!</h1>");
 });
 
 app.delete("/", (req, res) => {
-    res.send("<h1>DELETE</h1>");
+    res.send("<h1>DELETE!</h1>");
 });
-
-
 
 const PORT = 5000;
 app.listen(PORT, ()=>{
