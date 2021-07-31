6c08aaa13ebefca20a46aa72524f160ffb34a23f
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-07-31 11:20:46 -0300

    Using Middleware

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 
+let consoleMethod = (req, res, next) => {
+    
+    console.log(req.method);
+    next();
+}
 
-app.get("/", (req, res) => {
-    console.log(req);
-    res.json({ name: "Joao"});
-});
+let hello = (req, res) => {
+    
+    res.send("Hello World");
+}
+
+app.use("/", consoleMethod);
+app.get("/", hello);
+
+app.post("/", consoleMethod, hello);
 
 app.listen(3000, () => console.log('Server Running...'));
