a7336e5784a5b5de6a5fa6e51db63318e7394820
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-07-31 10:57:55 -0300

    Request using HTTP

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-app.use("/meusite", express.static(path.join(__dirname, 'client')));
 
 app.get("/", (req, res) => {
-    
-    res.send("<h1>Hello GET</h1>");
+    console.log(req);
+    res.send("<h1>Hello W GET</h1>");
 });
 
-app.post("/", (req, res) => {
-    res.send("<h1>POST!</h1>");
-});
-
-app.put("/", (req, res) => {
-    res.send("<h1>PUT!</h1>");
-});
-
-app.delete("/", (req, res) => {
-    res.send("<h1>DELETE!</h1>");
-});
-
-const PORT = 5000;
-app.listen(PORT, ()=>{
-    console.log(`Server Running on Port:${PORT}`)
-});
+app.listen(3000, () => console.log('Server Running...'));
