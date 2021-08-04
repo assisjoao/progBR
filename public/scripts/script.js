fee2ced82f31b516fcc91e5230db8ff1ef1db8d4
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-08-04 08:51:15 -0300

    Project ready!

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,4 @@
+const { options } = require("../../routes/api");
 
 document.addEventListener('DOMContentLoaded', () => {
     updatePosts();
@@ -5,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function updatePosts(){
 
-    fetch("http://localhost:3000/api/all").then(res => { 
+    fetch("http://localhost:3001/api/all").then(res => { 
         return res.json()
     }).then(json => {
 
@@ -30,5 +31,20 @@ function updatePosts(){
 
 function newPost(){
 
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("desc").value;
+
+    let post = {title, description};
+
+    const options = {method:"POST", 
+                    headers: new Headers({'content-type': 'application/json'}),
+                    body: JSON.stringify(post)}
+
+    fetch("http://localhost:3001/api/new", options).then(res => {
+        console.log(res);
+        updatePosts();
+        document.getElementById("title").value = "";
+        document.getElementById("desc").value = "";
+    })
 
 }
