65404c4dd7e253afc714290e326eaee66ca83836
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-08-07 12:48:47 -0300

    Changing variables with EJS

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+let user = {
+    name: 'João C',
+    phone: '(23) 23232323'
+}
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
-    res.render('user');
+    res.render('user', user);
 
     
 });
