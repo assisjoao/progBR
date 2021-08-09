5d1b8aaf9f2ad7315c5703ed58e7b06f8dd28820
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-08-09 09:55:51 -0300

    Init React

--- index.js
+++ index.js
@@ -1,45 +1,6 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const app = express();
+const root = document.querySelector("#root");
 
-let users = [
-    {
-        id: 0,
-        name: 'João C',
-        phone: '(23) 23232323'
-    },
-    {
-        id: 1,
-        name: 'José',
-        phone: '(23) 23232323'
-    },
-    {
-        id: 2,
-        name: 'Marcio',
-        phone: '(23) 23232323'
-    },
-]
+const element = React.createElement("h1", {id:"Título principal"}, "Helloooooo");
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-
-    res.render('user', {users});
-
-    
-
-    
-});
-
-app.get('/about', (req, res) => {
-
-    res.render('about');
-
-    
-});
-
-app. listen (3000, () => {
-    console.log('Server Running on 3000')
-});
+ReactDOM.render(element, root);
