2e8bbf7b826aa83a768b0f26616e2e4dd105a9d4
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-08-12 08:10:12 -0300

    Using States

--- index.js
+++
@@ -1,9 +0,0 @@
-const Person = require('./Person');
-const Alumn = require('./Alumn');
-
-
-let marcos = new Alumn("Marcos", 31, 1002);
-marcos.greatings();
-
-let jose = new Person("José", 31);
-jose.greatings();
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
 import Header from './Header';
+import Counter from './Counter';
+import './App.css';
 
 function App() {
   return (
+    <div>
     <Header name="JC" links={["Sobre", "Comprar", "Contato", "Login"]}></Header>
+    <Counter count={5}></Counter>
+    </div>
   );
 }
 
