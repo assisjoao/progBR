05f621e236b50e097957f0bf4df9a12958d49bda
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-08-13 08:43:02 -0300

    More Hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header';
 import Counter from './Counter';
 import './App.css';
 
 function App() {
-  return (
+
+  const [show, setShow] = useState(true);
+
+  let time = 0;
+
+  useEffect(()=>{
+    let timer = setInterval(()=>{
+      time++;
+      if(time > 5){
+        clearInterval(timer);
+        setShow(false);
+      }
+    }, 1000)
+  },[])
+
+  
+
+
+  if (show){
+    return (
     <div>
     <Counter count={3}></Counter>
     </div>
   );
+  }else{
+    return (
+      <div>
+      Sem contador
+      </div>
+    );
+  }
 }
 
 export default App;
