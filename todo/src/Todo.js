d08e39ea5acc4c22a731379ebc8ec5d427ef2d5e
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-08-13 20:53:17 -0300

    Putting the itens

--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -4,23 +4,30 @@ import './Todo.css'
 function Todo(){
 
     const [text, setText] = useState('');
+    const [itens, setItens] = useState([]);
 
     function handleChange(event) {
         let t = event.target.value;
         setText(t);
     }
 
-
+    function addItem(event) {
+        event.preventDefault();
+        if(text){
+            setItens([...itens, text]);
+            setText('');
+        };
+    }
 
     return (<div className='container'>
                 <h1>Todo</h1>
                 <form>
-                    <input onChange={handleChange} type='text'></input>
-                    <button>Add</button>
+                    <input onChange={handleChange} type='text' value={text}></input>
+                    <button onClick={addItem}>Add</button>
                 </form>
 
                 <ul>
-                    <li>{text}</li>
+                    {itens.map(item=><li>{item}</li>)}
                 </ul>
             </div>);
 
