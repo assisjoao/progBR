1365ed7a1ebc463cc314a4a0c9e092add0d4b672
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-08-15 11:02:12 -0300

    Sharing the components and separating the Form

--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -1,36 +1,30 @@
 import React, { useState } from 'react'
 import List from './List'
+import TodoForm from './TodoForm'
 import './Todo.css'
+
+
+
 
 function Todo(){
 
-    const [text, setText] = useState('');
     const [itens, setItens] = useState([]);
 
-    function handleChange(event) {
-        let t = event.target.value;
-        setText(t);
-    }
-
-    function addItem(event) {
-        event.preventDefault();
-        if(text){
-            setItens([...itens, text]);
-            setText('');
-        };
+    function onAddItem(item){
+        setItens([...itens, item])
     }
 
     return (<div className='container'>
                 <h1>Todo</h1>
-                <form>
-                    <input onChange={handleChange} type='text' value={text}></input>
-                    <button onClick={addItem}>Add</button>
-                </form>
+                <TodoForm onAddItem={onAddItem}></TodoForm>
+                
 
                 <List itens={itens}></List>
             </div>);
 
 }
+
+
 
 
 
